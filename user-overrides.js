69367bd2b87e242a45c307c0a65d413602fa1079
Khanh Hien Hoang <kh4nhhi3n@hotmail.com>
2023-06-20 10:51:55 +0000

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,14 +1,5 @@
 // My overrides
 
-// Change startup page to homepage instead of blank page
-user_pref("browser.startup.page", 1);
-
-// Set homepage to Firefox Home
-user_pref("browser.startup.homepage", "about:home");
-
-// Add new tab page
-user_pref("browser.newtabpage.enabled", true);
-
 // Disable auto clear cookies after quit Firefox because nobody wants to login again after open Firefox
 user_pref("privacy.clearOnShutdown.cookies", false);
 
