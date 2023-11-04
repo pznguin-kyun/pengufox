93819afcc769beead5036cc6494f4bf0c464ec9e
p3nguin-kun <p3nguinkun@proton.me>
2023-11-04 19:51:33 +0700

    update

--- penguinfox-installer.sh
+++ penguinfox-installer.sh
@@ -16,8 +16,6 @@ date=$(date +%Y%m%d-%H%M%S)
 logo() {
 
 	local text="${1:?}"
-	echo -en "                                 
-  	penguinFox Installer\n\n"
 	printf ' %s [%s%s %s%s %s]%s\n\n' "${CRE}" "${CNC}" "${CYE}" "${text}"
 }
 
@@ -25,16 +23,6 @@ logo() {
 clear
 logo "Welcome!"
 printf '%s%sThis script will automatically install penguinFox - my Firefox config to your system %s\n\n' "${BLD}" "${CRE}" "${CNC}"
-
-while true; do
-	read -rp " Do you want to continue? [y/n]: " yn
-	case $yn in
-	[Yy]*) break ;;
-	[Nn]*) exit ;;
-	*) printf "Just write 'y' or 'n'\n\n" ;;
-	esac
-done
-clear
 
 # Backup config files
 logo "Backing-up your config files"
@@ -45,39 +33,23 @@ if [ ! -d "$backup_folder" ]; then
 fi
 
 for folder in chrome; do
-	if [ -d "$HOME"/.mozilla/firefox/*.default-release/$folder ]; then
-		mv "$HOME"/.mozilla/firefox/*.default-release/$folder "$backup_folder"/${folder}_$date
-		echo "$folder folder backed up successfully at $backup_folder/${folder}_$date"
-	else
-		echo "The folder $folder does not exist in $HOME/.mozilla/firefox/"
+	if [ -d "$HOME"/.mozilla/firefox/*.default-*/$folder ]; then
+		mv "$HOME"/.mozilla/firefox/*.default-*/$folder "$backup_folder"/${folder}_$date
 	fi
 done
 
 for file in user.js; do
-	if [ -e "$HOME"/.mozilla/firefox/*.default-release/$file ]; then
-		mv "$HOME"/.mozilla/firefox/*.default-release/$file "$backup_folder"/${file}_$date
-		echo "$file file backed up successfully at $backup_folder/${file}_$date"
-	else
-		echo "The file $file does not exist in $HOME/.mozilla/firefox/"
+	if [ -e "$HOME"/.mozilla/firefox/*.default-*/$file ]; then
+		mv "$HOME"/.mozilla/firefox/*.default-*/$file "$backup_folder"/${file}_$date
 	fi
 done
 
 # Installing dotfiles
 logo "Installing penguinFox..."
-printf "Copying files to respective directories..\n"
-
-for archivos in ./*; do
-	cp -R "${archivos}" ~/.mozilla/firefox/*.default-release/
-	if [ $? -eq 0 ]; then
-		printf "%s%s%s copied succesfully!%s\n" "${BLD}" "${CGR}" "${archivos}" "${CNC}"
-	else
-		printf "%s%s%s failed to been copied, you must copy it manually%s\n" "${BLD}" "${CRE}" "${archivos}" "${CNC}"
-		sleep 1
-	fi
-done
+cp -R ./* ~/.mozilla/firefox/*.default-release/
 
 logo "Removing unused files..."
-rm -rf ~/.mozilla/firefox/*.default-release/.git ~/.mozilla/firefox/*.default-release/.github ~/.mozilla/firefox/*.default-release/penguinfox-installer.sh ~/.mozilla/firefox/*.default-release/README.md
+rm -rf ~/.mozilla/firefox/*.default-*/.git ~/.mozilla/firefox/*.default-*/.github ~/.mozilla/firefox/*.default-*/penguinfox-installer.sh ~/.mozilla/firefox/*.default-*/README.md
 
 logo "Done!"
 printf "Completed penguinFox installation, now open Firefox and enjoy!\n"
